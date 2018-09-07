1304253811981495b2e214f71c7d2a04e638c23d
A13k2 <alexander.drossel@protonmail.com>
2018-09-07 11:31:50 +0200

    added exception for tmux

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
-set -x SVN_EDITOR vim
-set -x EDITOR vim
-set -x PATH $PATH /home/alex/anaconda3/bin
+if not set -q TMUX
+	set -x SVN_EDITOR vim
+	set -x EDITOR vim
+	set -x PATH /home/alex/anaconda3/bin $PATH
+	set -x PYTHONPATH /home/alex/nest/nest2.16/lib/python3.6/site-packages/ $PYTHONPATH
+end
