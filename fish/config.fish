31fc22102b99e511fb176938d2e37189a2efe885
A13k2 <alexander.drossel@protonmail.com>
2018-12-29 17:06:15 +0100

    pythonpath nest

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,6 @@ if not set -q TMUX
 	set -x SVN_EDITOR vim
 	set -x EDITOR vim
 	set -x PATH /home/alex/anaconda3/bin $PATH
-	set -x PYTHONPATH /home/alex/nest/nest2.16/lib/python3.6/site-packages/ $PYTHONPATH
+	set -x PYTHONPATH /home/alex/nest_install/lib64/python3.7/site-packages/ $PYTHONPATH
 	set -gx PATH ~/.gem/ruby/2.5.0/bin $PATH
 end
