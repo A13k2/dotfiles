89d7baa4f3f19190bfd5ba6ac5c0f0df095f79f9
A13k2 <alexander.drossel@protonmail.com>
2018-12-29 17:48:45 +0100

    added nvim config that refers to vim config

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ ln -sv ~/dotfiles/.vimrc ~
 ln -sv ~/dotfiles/.bashrc ~
 ln -sv ~/dotfiles/fish ~/.config/
 ln -sv ~/dotfiles/ranger ~/.config/
+ln -sv ~/dotfiles/nvim ~/.config/
 ln -sv ~/dotfiles/.gitconfig ~
 ln -sv ~/dotfiles/.tmux.conf ~
 ln -sv ~/dotfiles/.i3 ~
